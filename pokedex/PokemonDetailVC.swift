9dea12fdbad964bd43daf2f5d8c67f36a74dbd4f
Muhammad Ali <muhammad.akbar.ali2008@gmail.com>
2016-05-05 21:28:33 -0400

    Added detail's VC

---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,37 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Muhammad Ali on 2016-05-05.
+//  Copyright © 2016 Muhammad Ali. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+    
+    var pokemon: Pokemon!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
